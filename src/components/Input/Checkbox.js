1c6a2cadbd00a3f23f0e2e21ab0c36880aa6641f
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-07-25 23:42:12 +0200

    [Components][Input][Checkbox] component created

---
+++ src/components/Input/Checkbox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { CheckCircle } from 'react-feather';
+
+const Checkbox = (props) => {
+    const { name, isChecked, event } = props
+
+    return ( 
+        <div className={`checkbox${ isChecked ? ' checkbox--active' : '' }`}>
+            <button 
+            className="checkbox__button"
+            onClick={() => {event(!isChecked)}}>
+              <CheckCircle className="checkbox__checkbox"></CheckCircle>
+              <h3 className="checkbox__title input-title">{name}</h3>
+            </button>
+        </div>
+    );
+}
+ 
+export default Checkbox;
