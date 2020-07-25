2db9316d4f65057e01ff0f7183787bfda4464897
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-07-25 23:41:03 +0200

    [Components][Terminal] Background Image and cursor options are added

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TerminalHeader from './TerminalHeader';
+import { useSelector } from 'react-redux';
+import defaultBackground from '../images/kity_space.gif'
 
-class Terminal extends Component {
-    state = {  }
-    render() { 
-        const { cursor } = this.props
-        return ( 
-            <div className="terminal">
-                <TerminalHeader/>
-                <div className="terminal__content">
-                    <p className="terminal__paragraph">Your Favorite Shell ❤</p>
-                    <p className="terminal__paragraph">C:\Users\me&gt; <span className="terminal__cursor">{cursor ? cursor : ' |'}</span></p>
-                </div>
-            </div>
-        );
-    }
+function Terminal() {
+  const cursor = useSelector(state => state.terminalReducer.cursor)
+  const regex = /(?<=\().+?(?=\))/g;
+  
+  return (
+    <div className="terminal">
+      <TerminalHeader />
+      <div className="terminal__content">
+        <div className="terminal__backgroundImage" style={{backgroundImage:`url(${defaultBackground})`}}></div>
+        <p className="terminal__paragraph">Your Favorite Shell ❤</p>
+        <p className="terminal__paragraph">C:\Users\me&gt; <span className="terminal__cursor">{cursor ? cursor.match(regex) : ' |'}</span></p>
+      </div>
+    </div>
+  );
 }
- 
+
 export default Terminal;
