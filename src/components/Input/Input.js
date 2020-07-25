d006b1f93776f77c72007677c9f5539b919b80a0
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-07-25 23:44:20 +0200

    [Components] Changed from component to functional component - Regex implemented

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Input extends Component {
-    state = {  }
-    render() { 
-        const { title } = this.props
-        return ( 
-            <div className="input">
-                <h3 className="input__title input-title">{title}</h3>
-                <input type="text" className="input__inputField"></input>
-            </div>
-        );
-    }
+const Input = (props) => {
+    const { title, event } = props
+
+    return ( 
+        <div className="input">
+            <h3 className="input__title input-title">{title}</h3>
+            <input type="text" className="input__inputField" onChange={(e) => {event(e)}}></input>
+        </div>
+    );
 }
  
 export default Input;
--- src/components/Settings/GeneralSettings.js
+++ src/components/Settings/GeneralSettings.js
@@ -1,21 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Input from '../Input/Input';
 import Dropdown from '../Input/Dropdown';
+import Checkbox from '../Input/Checkbox';
+import { useDispatch, useSelector } from 'react-redux'
+import { setTitle, setCursor, setScrollbarVisibility } from '../../actions'
 
-class GeneralSettings extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div className="general-settings">
-                <h2 className="general-settings__title settings-title">General Settings</h2>
-                <div className="general-settings__content content">
-                    <Input title="Tab Title"></Input>
-                    <Dropdown title="Font Face" options={['test1', 'test2', 'test3']}></Dropdown>
+const GeneralSettings = () => {
+  const fontFaceOptions = ['test1', 'test2', 'test3']
+  const cursorOptions = ['bar ( ┃ )', 'vintage ( ▃ )', 'underscore ( ▁ )', 'filledBox ( █ )', 'emptyBox ( ▯ )']
 
-                </div>
-            </div>
-         );
-    }
+  const dispatch = useDispatch();
+  const isScrollbarVisible = useSelector(state => state.terminalReducer.scrollbarVisibility)
+  const cursor = useSelector(state => state.terminalReducer.cursor)
+
+  const onTabTitleChange = (e) => {
+    dispatch(setTitle(e.target.value))
+  }
+
+  const onFontFaceChange = (value) => {
+    dispatch(setTitle(value))
+  }
+
+  const onFontWeightChange = (value) => {
+    dispatch(setTitle(value))
+  }
+
+  const onCursorChange = (value) => {
+    console.log(value)
+    dispatch(setCursor(value))
+  }
+
+  const onCloseOnExitChange = (value) => {
+    dispatch(setTitle(value))
+  }
+
+  const onScrollbarVisibilityChange = (value) => {
+    dispatch(setScrollbarVisibility(value))
+  }
+
+  return (
+    <div className="general-settings">
+      <h2 className="general-settings__title settings-title">General Settings</h2>
+      <div className="general-settings__content content">
+        <Input title="Tab Title" event={onTabTitleChange}></Input>
+        <Dropdown title="Font Face" options={fontFaceOptions}></Dropdown>
+        <Dropdown title="Font Weight" options={fontFaceOptions}></Dropdown>
+        <Dropdown title="Cursor" options={cursorOptions} activeOption={cursor} event={onCursorChange}></Dropdown>
+        <Dropdown title="Close on Exit" options={fontFaceOptions}></Dropdown>
+        <Checkbox name="Scrollbar visibility" isChecked={isScrollbarVisible} event={onScrollbarVisibilityChange}></Checkbox>
+      </div>
+    </div>
+  );
 }
- 
+
 export default GeneralSettings;
