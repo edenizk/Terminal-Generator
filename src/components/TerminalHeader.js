c479446072a6ce1c0e3f6e2fe80b888d7c0f3848
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-07-25 23:41:43 +0200

    [Components][Terminal Header] icon and title added from storage

--- src/components/TerminalHeader.js
+++ src/components/TerminalHeader.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react';
-// import feather from 'feather-icons'
+import React from 'react';
 import { X, Slash, Plus, ChevronDown, Minus, Square } from 'react-feather';
+import { useSelector } from 'react-redux';
 
-class TerminalHeader extends Component {
+function TerminalHeader() {
+  const icon = useSelector(state => state.terminalReducer.icon)
+  const title = useSelector(state => state.terminalReducer.tabTitle)
 
-    state = {  }
-    render() { 
-      const { icon, title } = this.props
-
-      return ( 
-        <div className="terminal-header">
-          <div className="terminal-header__tab">
-            {
-              icon ? <img className="terminal-header__icon" src={icon} alt="tab icon"></img>  : 
-                                <Slash className="terminal-header__icon terminal-header__icon--missing"></Slash>
-            }
-            <h2 className="terminal-header__title">{title ? title : 'Windows PowerShell'}</h2>
-            <X className="terminal-header__close"/>
-          </div>
-          <Plus className="terminal-header__add"/>
-          <ChevronDown className="terminal-header__down"/>
-          <div className="terminal-header__windowTools">
-            <Minus className="terminal-header__minimize"/>
-            <Square className="terminal-header__screenSize"/>
-            <X className="terminal-header__closeWindow"/>
-          </div>
-        </div>  
-      );
-    }
+  return ( 
+    <div className="terminal-header">
+      <div className="terminal-header__tab">
+        {
+          icon ? <img className="terminal-header__icon" src={icon} alt="tab icon"></img>  : 
+                <Slash className="terminal-header__icon terminal-header__icon--missing"></Slash>
+        }
+        <h2 className="terminal-header__title">{title ? title : 'Windows PowerShell'}</h2>
+        <X className="terminal-header__close"/>
+      </div>
+      <Plus className="terminal-header__add"/>
+      <ChevronDown className="terminal-header__down"/>
+      <div className="terminal-header__windowTools">
+        <Minus className="terminal-header__minimize"/>
+        <Square className="terminal-header__screenSize"/>
+        <X className="terminal-header__closeWindow"/>
+      </div>
+    </div>  
+  );
 }
  
 export default TerminalHeader;
