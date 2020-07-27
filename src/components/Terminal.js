13609a48482722ef234956274037a61b3ee47736
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-07-27 19:40:42 +0200

    [Components][Terminal] Logic added for bg Image Opacity

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -5,13 +5,20 @@ import defaultBackground from '../images/kity_space.gif'
 
 function Terminal() {
   const cursor = useSelector(state => state.terminalReducer.cursor)
+  const backgroundImageOpacity = useSelector(state => state.terminalReducer.backgroundImageOpacity)
+
   const regex = /(?<=\().+?(?=\))/g;
   
   return (
     <div className="terminal">
       <TerminalHeader />
       <div className="terminal__content">
-        <div className="terminal__backgroundImage" style={{backgroundImage:`url(${defaultBackground})`}}></div>
+        <div 
+        className="terminal__backgroundImage" 
+        style={{
+          backgroundImage:`url(${defaultBackground})`,
+          opacity:backgroundImageOpacity
+        }}></div>
         <p className="terminal__paragraph">Your Favorite Shell ❤</p>
         <p className="terminal__paragraph">C:\Users\me&gt; <span className="terminal__cursor">{cursor ? cursor.match(regex) : ' |'}</span></p>
       </div>
