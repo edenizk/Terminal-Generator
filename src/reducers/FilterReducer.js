ba2ff771036ea5f067b211b801c76f0523697c5c
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-07-27 19:43:43 +0200

    [Reducers][Filter Reducer] reducer created

---
+++ src/reducers/FilterReducer.js
@@ -0,0 +1,17 @@
+const INITIAL_STATE = Object.freeze({
+  filterIndex: 0
+});
+
+const filterReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SET_FILTER_INDEX':
+      return {
+        ...state,
+        filterIndex: action.payload,
+      }; 
+    default:
+      return state;
+  }
+}
+
+export default filterReducer;
