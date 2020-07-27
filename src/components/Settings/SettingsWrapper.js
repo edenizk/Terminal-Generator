b25535e969580e0b220de53f7c5a036e07b9e81d
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-07-27 19:45:21 +0200

    [Settings][SettingsWrapper] Basic filter logic added

--- src/components/Settings/SettingsWrapper.js
+++ src/components/Settings/SettingsWrapper.js
@@ -1,17 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Filter from './Filter';
+import { useSelector } from 'react-redux'
 import GeneralSettings from './GeneralSettings';
+import Background from './Background';
+import Padding from './Padding';
+import ColorSettings from './ColorSettings';
+import KeyboardSettings from './KeyboardSettings';
+import ScrollSettings from './ScrollSettings';
 
-class SettingsWrapper extends Component {
-    state = {  }
-    render() { 
-        return (
-            <div className="settings-wrapper">
-                <Filter/>
-                <GeneralSettings></GeneralSettings>
-            </div>
-        );
+const SettingsWrapper = () => {
+    const filterIndex = useSelector(state => state.filterReducer.filterIndex)
+
+    const getCurrentSection = () => {
+        switch (filterIndex) {
+            case 0:
+                return <GeneralSettings></GeneralSettings>
+            case 1:
+                return <Background></Background>
+            case 2:
+                return <Padding></Padding>
+            case 3:
+                return <ColorSettings></ColorSettings> 
+            case 4:
+                return <KeyboardSettings></KeyboardSettings>
+            case 5:
+                return <ScrollSettings></ScrollSettings>
+            default:
+                break;
+        }
     }
+    return (
+        <div className="settings-wrapper">
+            <Filter/>
+            {
+               getCurrentSection()
+            }
+        </div>
+    );
+    
 }
  
 export default SettingsWrapper;
