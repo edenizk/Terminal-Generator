bac358b7c353684c4caf1621690c0779e3ce1ae1
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-07-27 19:44:19 +0200

    [Reducers][Terminal Reducer] other settings are added to reducer

--- src/reducers/TerminalReducer.js
+++ src/reducers/TerminalReducer.js
@@ -4,8 +4,21 @@ const INITIAL_STATE = Object.freeze({
     fontWeight: 'Normal',
     cursor: 'bar ( ┃ )',
     closeOnExit: 'graceful',
+    icon: '',
+    backgroundImage: '',
+    backgroundImageOpacity: 0.5,
+    backgroundImageAlignment: 'Center',
+    backgroundImageStretch: 'None',
+    backgroundColor: '#472148',
+    enableAcrylic: true,
+    transparency: 0.5,
+    paddingTop: 30,
+    paddingBottom: 30,
+    paddingLeft: 30,
+    paddingRight: 30,
     scrollbarVisibility: true,
-    icon: ''
+    scrollToInput: true,
+    historySize: 9001
 });
   
 
@@ -31,15 +44,80 @@ const terminalReducer = (state = INITIAL_STATE, action) => {
           ...state,
           closeOnExit: action.payload,
         };  
-      case 'SET_SCROLLBAR_VISIBILITY':
-        return {
-          ...state,
-          scrollbarVisibility: action.payload,
-        };  
       case 'SET_ICON':
         return {
           ...state,
           icon: action.payload,
+        };
+      case 'SET_BACKGROUND_IMAGE':
+        return {
+          ...state,
+          backgroundImage: action.payload,
+        };  
+      case 'SET_BACKGROUND_IMAGE_OPACITY':
+        return {
+          ...state,
+          backgroundImageOpacity: action.payload,
+        };
+      case 'SET_BACKGROUND_IMAGE_ALIGNMENT':
+        return {
+          ...state,
+          backgroundImageAlignment: action.payload,
+        };
+      case 'SET_BACKGROUND_IMAGE_STRETCH':
+        return {
+          ...state,
+          backgroundImageStretch: action.payload,
+        };
+      case 'SET_BACKGROUND_COLOR':
+        return {
+          ...state,
+          backgroundColor: action.payload,
+        };
+      case 'SET_ENABLE_ACRYLIC':
+        return {
+          ...state,
+          enableAcrylic: action.payload,
+        };
+      case 'SET_TRANSPARENCY':
+        return {
+          ...state,
+          transparency: action.payload,
+        };
+      case 'SET_PADDING_TOP':
+        return {
+          ...state,
+          paddingTop: action.payload,
+        };
+      case 'SET_PADDING_BOTTOM':
+        return {
+          ...state,
+          paddingBottom: action.payload,
+        };
+    case 'SET_PADDING_LEFT':
+      return {
+        ...state,
+        paddingLeft: action.payload,
+      };
+      case 'SET_PADDING_RIGHT':
+        return {
+          ...state,
+          paddingRight: action.payload,
+        };
+      case 'SET_SCROLLBAR_VISIBILITY':
+        return {
+          ...state,
+          scrollbarVisibility: action.payload,
+        }; 
+      case 'SET_SCROLL_TO_INPUT':
+        return {
+          ...state,
+          scrollToInput: action.payload,
+        };
+      case 'SET_HISTORY_SIZE':
+        return {
+          ...state,
+          historySize: action.payload,
         };  
       default:
         return state;
