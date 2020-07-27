36f6736881ad784cebd7269aa7d79c2fcd658c35
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-07-27 19:44:46 +0200

    [Components][Settings] Basic Filter logic added

--- src/components/Settings/Filter.js
+++ src/components/Settings/Filter.js
@@ -1,34 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import { Sliders, Image, Crop, Command, Sidebar } from 'react-feather';
+import { setFilterIndex } from '../../actions/index'
 
-class Filter extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div className="filter">
-                <ul className="filter__icons">
-                    <li className="filter__icon">
-                        <Sliders/>
-                    </li>
-                    <li className="filter__icon">
-                        <Image/>
-                    </li>
-                    <li className="filter__icon">
-                        <Crop/>
-                    </li>
-                    <li className="filter__icon">
-                        <div className="filter__colorWheel"></div>
-                    </li>
-                    <li className="filter__icon">
-                        <Command/>
-                    </li>
-                    <li className="filter__icon">
-                        <Sidebar/>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+const Filter = () => {
+  const filterIndex = useSelector(state => state.filterReducer.filterIndex)
+  const dispatch = useDispatch();
+  const [activaIcon, setActivaIcon] = useState(0);
+
+  const filterIndexHook = (value) => {
+    dispatch(setFilterIndex(value))
+    setActivaIcon(value)
+  }
+
+  return (
+    <div className="filter">
+      <ul className="filter__icons">
+        <li className={`filter__icon${ filterIndex === 0 ? ' filter__icon--active' : '' }`} onClick={() => filterIndexHook(0)}>
+          <Sliders />
+        </li>
+        <li className={`filter__icon${ filterIndex === 1 ? ' filter__icon--active' : '' }`} onClick={() => filterIndexHook(1)}>
+          <Image />
+        </li>
+        <li className={`filter__icon${ filterIndex === 2 ? ' filter__icon--active' : '' }`} onClick={() => filterIndexHook(2)}>
+          <Crop />
+        </li>
+        <li className={`filter__icon${ filterIndex === 3 ? ' filter__icon--active' : '' }`} onClick={() => filterIndexHook(3)}>
+          <div className="filter__colorWheel"></div>
+        </li>
+        <li className={`filter__icon${ filterIndex === 4 ? ' filter__icon--active' : '' }`} onClick={() => filterIndexHook(4)}>
+          <Command />
+        </li>
+        <li className={`filter__icon${ filterIndex === 5 ? ' filter__icon--active' : '' }`} onClick={() => filterIndexHook(5)}>
+          <Sidebar />
+        </li>
+      </ul>
+    </div>
+  );
+
 }
- 
+
 export default Filter;
