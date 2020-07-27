7d50b583973aef03affd738886bf30c993174806
Ergin Deniz Kosecioglu <35309972+edenizk@users.noreply.github.com>
2020-07-27 19:39:52 +0200

    [Actions][Index] actions added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
+export const setFilterIndex = (value) => {
+  return {
+    type: 'SET_FILTER_INDEX',
+    payload: value
+  }
+}
+
 export const setTitle = (title) => {
   return {
     type: 'SET_TITLE',
@@ -26,6 +33,90 @@ export const setCloseOnExit = (value) => {
   }
 }
 
+export const setIcon = (value) => {
+  return {
+    type: 'SET_ICON',
+    payload: value
+  }
+}
+
+export const setBackgroundImage = (value) => {
+  return {
+    type: 'SET_BACKGROUND_IMAGE',
+    payload: value
+  }
+}
+
+export const setBackgroundImageOpacity = (value) => {
+  return {
+    type: 'SET_BACKGROUND_IMAGE_OPACITY',
+    payload: value
+  }
+}
+
+export const setBackgroundImageAlignment = (value) => {
+  return {
+    type: 'SET_BACKGROUND_IMAGE_ALIGNMENT',
+    payload: value
+  }
+}
+
+export const setBackgroundImageStretch = (value) => {
+  return {
+    type: 'SET_BACKGROUND_IMAGE_STRETCH',
+    payload: value
+  }
+}
+
+export const setBackgroundColor = (value) => {
+  return {
+    type: 'SET_BACKGROUND_COLOR',
+    payload: value
+  }
+}
+
+export const setEnableAcrylic = (value) => {
+  return {
+    type: 'SET_ENABLE_ACRYLIC',
+    payload: value
+  }
+}
+
+export const setTransparency = (value) => {
+  return {
+    type: 'SET_TRANSPARENCY',
+    payload: value
+  }
+}
+
+export const setPaddingTop = (value) => {
+  return {
+    type: 'SET_PADDING_TOP',
+    payload: value
+  }
+}
+
+export const setPaddingBottom = (value) => {
+  return {
+    type: 'SET_PADDING_BOTTOM',
+    payload: value
+  }
+}
+
+export const setPaddingLeft = (value) => {
+  return {
+    type: 'SET_PADDING_LEFT',
+    payload: value
+  }
+}
+
+export const setPaddingRight = (value) => {
+  return {
+    type: 'SET_PADDING_RIGHT',
+    payload: value
+  }
+}
+
 export const setScrollbarVisibility = (value) => {
   return {
     type: 'SET_SCROLLBAR_VISIBILITY',
@@ -33,9 +124,16 @@ export const setScrollbarVisibility = (value) => {
   }
 }
 
-export const setIcon = (value) => {
+export const setScrollToInput = (value) => {
   return {
-    type: 'SET_ICON',
+    type: 'SET_SCROLL_TO_INPUT',
+    payload: value
+  }
+}
+
+export const setHistorySize = (value) => {
+  return {
+    type: 'SET_HISTORY_SIZE',
     payload: value
   }
 }
