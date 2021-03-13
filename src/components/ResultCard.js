0e379cec4bb9e86a38aed9f87c6f49b453f17b7c
Ergin Deniz Kosecioglu <deniz@crafton.pl>
2021-03-13 22:59:07 +0100

    basic styling and function added to result card

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import SettingsTemplate from './SettingsTemplate';
+import AdvanceSettingsTemplate from './AdvanceSettingsTemplate';
+import { ChevronsUp } from 'react-feather';
 
-function ResultCard() {
+class ResultCard extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {isClose: true};
+
+    this.toggleCard = this.toggleCard.bind(this);
+  }
+
+  toggleCard() {
+    this.setState(state => ({
+      isClose: !state.isClose
+    }));
+  }
   
-  return (
-    <div className="resultCard">
-      <SettingsTemplate />
-    </div>
-  );
+  render() {
+    return (
+      <div className={`resultCard resultCard--${this.state.isClose && 'closed'}`}>
+        <div className="resultCard__toggle" onClick={this.toggleCard}>
+          <ChevronsUp className="resultCard__toggleIcon"></ChevronsUp>
+            <h4 className="resultCard__toggleText">{this.state.isClose ? 'Done?' : 'Close'}</h4>
+          <ChevronsUp nsUp className="resultCard__toggleIcon"></ChevronsUp>
+        </div>
+        <AdvanceSettingsTemplate />
+      </div>
+    );
+  }
 }
 
 export default ResultCard;
