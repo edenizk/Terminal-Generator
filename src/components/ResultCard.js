3686d38a177b8526800b1e68c5d4ac5aed1e8393
Ergin Deniz Kosecioglu <deniz@crafton.pl>
2021-03-15 17:44:12 +0100

    component class turned to functional component

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,33 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AdvanceSettingsTemplate from './AdvanceSettingsTemplate';
 import { ChevronsUp } from 'react-feather';
 
-class ResultCard extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {isClose: true};
+const ResultCard = () => {
+  const [isClose, setIsClose] = useState(true);
 
-    this.toggleCard = this.toggleCard.bind(this);
-  }
-
-  toggleCard() {
-    this.setState(state => ({
-      isClose: !state.isClose
-    }));
+  const toggleCard = () => {
+    setIsClose(!isClose);
   }
   
-  render() {
-    return (
-      <div className={`resultCard resultCard--${this.state.isClose && 'closed'}`}>
-        <div className="resultCard__toggle" onClick={this.toggleCard}>
-          <ChevronsUp className="resultCard__toggleIcon"></ChevronsUp>
-            <h4 className="resultCard__toggleText">{this.state.isClose ? 'Done?' : 'Close'}</h4>
-          <ChevronsUp nsUp className="resultCard__toggleIcon"></ChevronsUp>
-        </div>
-        <AdvanceSettingsTemplate />
+  return (
+    <div className={`resultCard resultCard--${isClose && 'closed'}`}>
+      <div className="resultCard__toggle" onClick={toggleCard}>
+        <ChevronsUp className="resultCard__toggleIcon"></ChevronsUp>
+          <h4 className="resultCard__toggleText">{isClose ? 'Done?' : 'Close'}</h4>
+        <ChevronsUp nsUp className="resultCard__toggleIcon"></ChevronsUp>
       </div>
-    );
-  }
+      <AdvanceSettingsTemplate />
+    </div>
+  )
 }
 
 export default ResultCard;
