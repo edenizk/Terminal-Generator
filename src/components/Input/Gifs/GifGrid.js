cb6a5f7f441c6d2530b4ec89e39b4e29d94e758f
edenizk <dkokyanus@gmail.com>
2021-03-21 19:06:28 +0100

    Basuc Gif component created

---
+++ src/components/Input/Gifs/GifGrid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GifGrid = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GifGrid;
---
+++ src/components/Input/Gifs/GifSelector.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GifSelector = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GifSelector;
