ff49da09735fec682e29a921ff1317ba2e47f3b5
edenizk <dkokyanus@gmail.com>
2021-03-21 19:06:07 +0100

    default value reducer created

---
+++ src/redux/reducers/DefaultValueReducer.js
@@ -0,0 +1,33 @@
+const INITIAL_STATE = Object.freeze({
+  name: 'test',
+  fontFace: 'Cascadia Mono',
+  fontWeight: 'Normal',
+  cursorShape: 'bar',
+  closeOnExit: 'graceful',
+  icon: '',
+  backgroundImage: '',
+  backgroundImageOpacity: 50,
+  backgroundImageAlignment: 'Center',
+  backgroundImageStretchMode: 'None',
+  backgroundColor: '#472148', // THIS SET AS ONLY BACKGROUND IN SETTINGS.JSON
+  useAcrylic: true,
+  acrylicOpacity: 50,
+  padding: {
+    top: 8,
+    right: 8,
+    bottom: 8,
+    left: 8
+  },
+  scrollbarVisibility: true, // Property name: scrollbarState
+  scrollToInput: true, // Property name: snapOnInput
+  historySize: 9001
+});
+
+const defaultValueReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default defaultValueReducer;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import terminalReducer from './TerminalReducer'
 import filterReducer from './FilterReducer'
+import defaultValueReducer from './DefaultValueReducer'
 import { combineReducers } from 'redux'
 
 const mainReducer = combineReducers({
   terminalReducer,
-  filterReducer
+  filterReducer,
+  defaultValueReducer,
 })
 
 export default mainReducer
