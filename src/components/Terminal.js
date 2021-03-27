75f9462d9de8f7c7c29d3b6fcaa12b6b42b2d75b
edenizk <dkokyanus@gmail.com>
2021-03-27 20:53:59 +0100

    code clean up

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -5,37 +5,40 @@ import defaultBackground from '../images/kity_space.gif';
 
 function Terminal() {
   const terminalReducer = useSelector(state => state.terminalReducer);
-  const cursorShape = useSelector(state => state.terminalReducer.cursorShape);
-  const backgroundImageOpacity = useSelector(state => state.terminalReducer.backgroundImageOpacity);
-  const padding = useSelector(state => state.terminalReducer.padding);
+  const cursorShape = terminalReducer.cursorShape;
   
   const cursorShapeOptions = {
     bar: '┃', 
-    vintage: ' ▃ ', 
-    underscore: ' ▁ )', 
+    vintage: '▃', 
+    underscore: '▁', 
     filledBox: '█', 
     emptyBox: '▯'
   };
 
-  const regex = /(?<=\().+?(?=\))/g;
   
   return (
     <div className="terminal">
       <TerminalHeader />
       <div className="terminal__content" style={{
-        paddingTop: padding.top,
-        paddingRight: padding.right,
-        paddingBottom: padding.bottom,
-        paddingLeft: padding.left
+        paddingTop: terminalReducer.padding.top,
+        paddingRight: terminalReducer.padding.right,
+        paddingBottom: terminalReducer.padding.bottom,
+        paddingLeft: terminalReducer.padding.left,
+        fontFamily: terminalReducer.fontFace,
+        fontWeight: terminalReducer.fontWeight,
       }}>
         <div 
           className="terminal__backgroundImage" 
           style={{
             backgroundImage:`url("${terminalReducer.backgroundImage ?? ''}")`,
-            opacity:terminalReducer.backgroundImageOpacity / 100
+            opacity:terminalReducer.backgroundImageOpacity / 100,
           }}></div>
         <p className="terminal__paragraph">Your Favorite Shell ❤</p>
-        <p className="terminal__paragraph">C:\Users\me&gt; <span className="terminal__cursorShape">{cursorShape ? cursorShapeOptions[cursorShape] : '|'}</span></p>
+        <p className="terminal__paragraph">C:\Users\me&gt; 
+          <span className="terminal__cursorShape">
+            {cursorShape ? cursorShapeOptions[cursorShape] : '|'}
+          </span>
+        </p>
       </div>
     </div>
   );
