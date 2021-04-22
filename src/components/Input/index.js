57af2181a41525ce1012519dd20e03f8c2ce32d6
edenizk <dkokyanus@gmail.com>
2021-04-22 22:23:11 +0200

    add Image component imports

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,3 +3,4 @@ export {default as Dropdown} from './Dropdown';
 export {default as Checkbox} from './Checkbox';
 export {default as SliderInput} from './SliderInput';
 export {default as GoogleFontsDropdown} from './GoogleFontsDropdown';
+export {default as AddImage} from './AddImage';
