88d8612c41b2a33807a5d0cdde9b3c4601638494
edenizk <dkokyanus@gmail.com>
2021-04-25 07:06:02 +0200

    contact added

--- src/components/GlobalHeader.js
+++ src/components/GlobalHeader.js
@@ -5,8 +5,18 @@ const GlobalHeader = (pros) => {
     
   return ( 
     <header className="global-header">
-      <img src={logo} className="global-header__logo" alt="logo" />
-      <h1 className="global-header__title">Terminal Generator</h1>
+      <div className="global-header__logoWrapper">
+        <img src={logo} className="global-header__logo" alt="logo" />
+        <h1 className="global-header__title">Terminal Generator</h1>
+      </div>
+      <nav className="global-header__contact">
+        <a className="global-header__repo link" href="https://github.com/edenizk/Terminal-Generator" target="_blank" rel="noopener noreferrer">
+          Project Repo
+        </a>
+        <a className="global-header__me link" href="https://edenizk.github.io/" target="_blank" rel="noopener noreferrer">
+          Contact Me
+        </a>
+      </nav>
     </header>
   );
 };
